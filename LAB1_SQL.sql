25850b13772a5873d37d15d44f252ed73e4c8fa0
Ayeesha Siddikka <ayeeshasiddikka@gmail.com>
2022-09-18 18:58:56 +0200

    sqllab

--- LAB1_SQL.sql
+++ LAB1_SQL.sql
@@ -27,23 +27,20 @@ FROM
 -- Note that we are not asking you to obtain the language per each film
 -- but this is a good time to think about how you might get that information in the future.
 
-SELECT distinct language_id from film;
+select distinct(name) as language from language;
 
 
 -- 5.1 Find out how many stores does the company have?
-SELECT count(store_id) from store;
+select count(distinct store_id) from store;
    
     
     -- 5.2 Find out how many employees staff does the company have?
     
-    SELECT COUNT(staff_id)
-FROM staff;
+select count(distinct staff_id) from staff;
     
     -- 5.3 Return a list of employee first names only?
 
-SELECT staff_id,
-FROM staff
-where in first_name ;
+select first_name from staff;
 
    
     
@@ -52,20 +49,23 @@ where in first_name ;
 use sakila;
 -- 1.Select all the actors with the first name ‘Scarlett’.
 
- select * from actor where first_name = 'Scarlett';
+ select first_name from actor
+where first_name = "Scarlett";
  
  -- 2.How many films (movies) are available for rent and how many films have been rented?
-SELECT COUNT(title) FROM sakila.film; 
- 
+
+SELECT COUNT(*) FROM rental;
+SELECT COUNT(*) FROM film;
 
  -- 3.What are the shortest and longest movie duration? Name the values max_duration and min_duration.
 
-SELECT MAX(length) as max_duration ,MIN(length) as min_duration from sakila.film;
+select min(length) as min_duration,max(length) as max_duration from film;
 
 
 -- 4.What's the average movie duration expressed in format (hours, minutes)?
 -- SELECT avg(length) as avg_movie_duration from sakila.film; 
-SELECT concat(floor((AVG(length))/60),":",round((AVG(length))%60,0)) AS "average_length" FROM sakila.film;
+SELECT SEC_TO_TIME(AVG(length)*60) as 'hours and min'
+FROM film;
       
 -- 5.How many distinct (different) actors' last names are there?
 
@@ -73,26 +73,25 @@ select count(distinct last_name) from actor;
 
 
 -- 6.Since how many days has the company been operating (check DATEDIFF() function)?
-SELECT DATEDIFF(MAX(last_update), MIN(rental_date))
+SELECT DATEDIFF(MAX(rental_date), MIN(rental_date)) as 'days_operating'
 FROM sakila.rental;
 
 
 -- 8.Add an additional column day_type with values 'weekend' and 'workday' depending on the rental day of the week.
 
 -- SELECT * ,DAYNAME(return_date) as day_type from rental;
+SELECT DATE_FORMAT(rental_date,'%M') AS 'month', DATE_FORMAT(rental_date,'%a') AS 'day'
+FROM rental
+LIMIT 20;
 
-SELECT *,
+select rental_date,
 CASE
-WHEN DAYNAME(rental_date) = 'Monday' then 'Workday'
-WHEN DAYNAME(rental_date) = 'Tuesday' then 'Workday'
-WHEN DAYNAME(rental_date) = 'Wednesday' then 'Workday'
-WHEN DAYNAME(rental_date) = 'Thursday' then 'Workday'
-WHEN DAYNAME(rental_date) = 'Friday' then 'Workday'
-WHEN DAYNAME(rental_date) = 'Saturday' then 'Weekend'
-WHEN DAYNAME(rental_date) = 'Sunday' then 'Weekend'
-ELSE 'No info'
-END AS 'day_type', DAYNAME(rental_date) AS 'weekday'
-FROM sakila.rental;
+WHEN DATE_FORMAT(rental_date,'%a') = 'Sat' then 'weekend'
+WHEN DATE_FORMAT(rental_date,'%a') = 'Sun' then 'weekend'
+ELSE 'weekday'
+END AS column_day_type
+FROM rental;
+
 -- 9.Get release years.
 
 SELECT title, release_year as 'release years' FROM sakila.film;
@@ -101,7 +100,7 @@ SELECT title, release_year as 'release years' FROM sakila.film;
 
 -- 10.Get all films with ARMAGEDDON in the title.
 
-select film_id,title from film where title like '%ARMAGEDDON%';
+select * from film where title like '%ARMAGEDDON%';
 
 
 -- 11.Get all films which title ends with APOLLO.
@@ -111,11 +110,10 @@ select film_id,title from film where title like '%APOLLO';
 SELECT title, length FROM sakila.film
 ORDER BY length DESC
 LIMIT 10;
-​
 
 -- 13.How many films include Behind the Scenes content?
 
- select film_id,special_features from film where special_features like '%Behind the scenes%';
+ select count(special_features) as 'Behind_the_Scenes_content' from film where special_features like '%Behind the scenes%';
  
  /*Lab | SQL Queries - Lesson 2.6 (dataV3_Lesson_2.6_lab)*/
  
@@ -136,22 +134,25 @@ LIMIT 10;
  select last_name from actor group by last_name having count(last_name) > 1;
  
  -- 3.Using the rental table, find out how many rentals were processed by each employee.
- 
 
+ SELECT COUNT(rental_id), staff_id
+FROM rental
+GROUP BY staff_id;
  
  
- select staff_id from rental group by staff_id having count(rental_id);
  
  -- 4.Using the film table, find out how many films were released each year.
  
 
-
- select film_id,count(release_year) from film  group by (release_year);
+ select count(film_id), release_year from film 
+ group by (release_year);
  
  -- 5.Using the film table, find out for each rating how many films were there.
 
+SELECT COUNT(film_id), rating
+FROM film
+GROUP BY rating;
 
-SELECT rating, COUNT(rating) AS 'number' FROM film GROUP BY rating;
 
  
  -- 6.What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
